fd6ab6ca696a49ec87ddf1a493de43b9ed5c8c5b
Laboratorio de Informática 6 - Pasto <labinf6.pasto@CAMPUS.local>
2026-05-14 17:58:56 -0500

    Segundo commit Bastidas

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from app.core.config import settings
+from .config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
